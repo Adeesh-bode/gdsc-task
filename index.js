ed52e9218d6ef4f631124e6f365431bb23960e9c
Adeesh-bode <121815318+Adeesh-bode@users.noreply.github.com>
2023-09-09 23:41:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,3 @@ const themetogglebtn = document.querySelector('.mode');
 themetogglebtn.addEventListener('click',() => {
    document.body.classList.toggle('dark-mode'); 
 })
-/*
-const techinfo = document.querySelector('.techdown');
-
-techinfo.addEventListener('click',()=>{
-    document.p.classList.toggle('visible');
-})
-*/
