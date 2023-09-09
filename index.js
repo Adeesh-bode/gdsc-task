87a1753549d2cc9699e00b8d0a29eee5a1a40a4f
Adeesh-bode <121815318+Adeesh-bode@users.noreply.github.com>
2023-09-09 21:23:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const themetogglebtn = document.querySelector('.mode');
+
+themetogglebtn.addEventListener('click',() => {
+   document.body.classList.toggle('dark-mode'); 
+})
+/*
+const techinfo = document.querySelector('.techdown');
+
+techinfo.addEventListener('click',()=>{
+    document.p.classList.toggle('visible');
+})
+*/
